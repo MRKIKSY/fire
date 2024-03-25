2ca19916c6bb34c8a17ddbe6a3d8f1fc757eec82
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-03-25 22:47:20 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
         </Route>
         <Route exact path="/login" component={() => <Login />}></Route>
         <Route exact path="/signup" component={() => <Register />}></Route>
-        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
     </div>
   );
